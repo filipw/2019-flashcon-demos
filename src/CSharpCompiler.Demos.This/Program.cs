8139d0b689ae111ebb58b5fedb7a681335b467a4
filipw <wojcieszyn@gmail.com>
2019-12-12 14:16:07 +0100

    more demos

---
+++ src/CSharpCompiler.Demos.This/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace CSharpCompiler.Demos.This
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Message msg = new Message();
+            msg.Print();
+
+            Console.ReadLine();
+
+            Ooops();
+        }
+
+        private static void Ooops()
+        {
+            Message msg2 = null;
+            var printer = (Action<Message>)typeof(Message).GetMethod("Print").CreateDelegate(typeof(Action<Message>));
+            printer.Invoke(msg2);
+        }
+    }
+
+    public class Message
+    {
+        public void Print()
+        {
+            var text = this == null ? "I don't know" : this.ToString();
+            Console.WriteLine("I am " + text + "!");
+        }
+    }
+}
